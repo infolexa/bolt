960a647a3cc5bea6426f0a0b59ff85b414577a00
Israel D. Canasa <raeldc@wizmediateam.com>
2010-02-02 14:25:07 +0800

    Removed Static HTML as module. It's too simple. So just add it to HoneyComb Core.

---
+++ application/logs/2010/02/02.php
@@ -0,0 +1,4 @@
+<?php defined('SYSPATH') or die('No direct script access.'); ?>
+
+2010-02-02 14:21:08 --- ERROR: Kohana_Request_Exception [ 0 ]: Unable to find a route to match the URI:  ~ SYSPATH/classes/kohana/request.php [ 574 ]
+2010-02-02 14:22:19 --- ERROR: ReflectionException [ -1 ]: Class controller_statichtml does not exist ~ SYSPATH/classes/kohana/request.php [ 848 ]
