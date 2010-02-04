4d3566b21e3dbc2ccf2ce61cf0a27c1985f66cb7
Israel D. Canasa <raeldc@wizmediateam.com>
2010-02-04 11:42:27 +0800

    Changed bootstrap

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -1,17 +1,20 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-//-- Environment setup --------------------------------------------------------
-
-date_default_timezone_set('Asia/Manila');
+/*-- Initialize Kohana --*/
 spl_autoload_register(array('Kohana', 'auto_load'));
+date_default_timezone_set('Asia/Manila');
 Kohana::init(array('base_url' => str_replace(basename($_SERVER['SCRIPT_NAME']),'',$_SERVER['SCRIPT_NAME']), 'index_file' => ''));
-//Kohana::$log->attach(new Kohana_Log_File(APPPATH.'logs'));
+
+/*-- Initialize Logging --*/
+Kohana::$log->attach(new Kohana_Log_File(APPPATH.'logs'));
 Kohana::$config->attach(new Kohana_Config_File);
+
+/*-- Load the 2 basic Modules first --*/
 Kohana::modules(array('database' => MODPATH.'database', 'honeycomb'  => MODPATH.'honeycomb'));
-
-
+/*-- Load everything else --*/
 Apps::load();
 
+/*-- Set the route that catches everything --*/
 Route::set('app/statichtml', '(<file>(.html))', array(
 		'file' => '[a-z\-]*+'
 	))
@@ -21,16 +24,38 @@ Route::set('app/statichtml', '(<file>(.html))', array(
 		'file'	 	 => 'welcome'
 	));
 
+/*
+echo Kohana::debug(Route::name(Request::instance()->route));
+die();
+*/
+
+/*
+echo Kohana::debug(Request::instance()->uri());
+die();
+*/
+
+/*
+echo Kohana::debug(Route::all());
+die();
+*/
+
+/*-- Check if Admin is being accessed --*/
+if (Request::instance()->param('mode') == 'admin') 
+{
+	$template = View::factory('admin/template');
+}else
+{
+	$template = View::factory('template');
+}
+
 $response = Request::instance()->execute()->send_headers()->response;
+
 if (Request::$is_ajax OR Request::instance()->action == 'media')
 {
 	echo $response;
 	exit;
 }
-
-$content = View::factory('template')->set('content', $response)->render();
+$content = $template->set('content', $response)->render();
 $head = HTML::head(TRUE);
 $token = HTML::token();
 echo str_replace('[HEAD-'.$token.']', $head, $content);
-
-
