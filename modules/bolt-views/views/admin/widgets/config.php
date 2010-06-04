012a87ed33fb824c4c749aee060565983db5ea78
Bolt CMS <bolt@wizmediateam.com>
2010-06-04 12:15:13 +0800

    Trying out an experimental file system structure

--- modules/bolt-views/views/admin/widgets/config.php
+++
@@ -1,8 +0,0 @@
-<?php defined('SYSPATH') or die('404 Not Found.');?>
-
-<?php foreach ($params as $key => $value): ?>
-	<p>
-		<label><?php echo ucwords($key) ?></label> <br />
-		<?php echo Form::input('params['.$key.']', $value) ?>
-	</p>
-<?php endforeach ?>
--- modules/bolt-views/views/statichtml/default.php
+++
@@ -1,3 +0,0 @@
-<?php defined('SYSPATH') or die('404 Not Found.'); ?>
-
-This is a static html.. <br />
